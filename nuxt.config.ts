fd11a3729449587a6affd3c13dab8922d96c503d
saeedkgr <54794474+saeedkgr@users.noreply.github.com>
2025-01-26 22:18:38 +0330

    Update nuxt.config.ts add d1 database

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   css: ['~/assets/css/main.css'],
   modules: ['@nuxt/content'],
+   content: {
+    database: {
+      type: 'd1',
+      binding: 'saeedkgr-weblog'
+    }
+  },
   devtools: { enabled: true },
 })
